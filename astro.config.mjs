ac737b0da5512197cb4a849b7518385abca5dd4c
mynamo101 <mynamo101@gmail.com>
2025-06-13 17:45:47 +0800

    更新到Elysian版本

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+// https://github.com/delucis/astro-auto-import
+import AutoImport from "astro-auto-import";
+import { remarkReadingTime } from './src/utils/remark-reading-time.mjs';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://elysian-astro.kusa-projects.com",
+    integrations: [
+        AutoImport({
+			imports: [
+				"@components/Button.astro",
+                "@components/ImageGallery.astro",
+                "@components/Callout.astro",
+                "@components/ToggleCards.astro",
+                "@components/MediaEmbed.astro"
+			],
+		}),
+        mdx(),
+        sitemap()
+    ],
+    markdown: {
+        remarkPlugins: [remarkReadingTime],
+    }
+});
