76da1f0b67dcec11065af98b704ba312985c5c5d
mynamo101 <mynamo101@gmail.com>
2025-06-14 09:56:30 +0800

    調整Navbar

--- src/config/navbar.json.ts
+++ src/config/navbar.json.ts
@@ -11,15 +11,14 @@ const navbarSettings: Navbar[] = [
 		text: "Home",
 		link: "/",
 	},
-    // {
-	// 	text: "Style Guide",
-	// 	link: "/style-guide/",
-	// },
-	// {
-	// 	text: "Documentation",
-	// 	link: "https://www.kusa-projects.com/astro-documentation",
-	// 	new_tab: true
-	// },
+	{
+		text: "Works",
+		link: "/archive/",
+	},
+	{
+		text: "Contact",
+		link: "/contact/",
+	},
 	{
 		text: "Language",
 		dropdown: [
@@ -35,65 +34,65 @@ const navbarSettings: Navbar[] = [
 			}
 		]
 	},
-	{
-		text: "Features",
-		dropdown: [
-			{
-				text: "Features",
-				link: "/features/",
-			},
-			{
-				text: "Archive",
-				link: "/archive/",
-			},
-			{
-				text: "Tags",
-				link: "/tags/",
-			},
-			{
-				text: "Authors",
-				link: "/authors/",
-			},
-			{
-				text: "Account",
-				link: "/account/",
-			},
-			{
-				text: "Subscribe",
-				link: "/subscribe/",
-			},
-			{
-				text: "Membership",
-				link: "/membership/",
-			},
-			{
-				text: "Contact",
-				link: "/contact/",
-			},
-			{
-				text: "FAQ",
-				link: "/faq/",
-			},
-			// {
-			// 	text: "404",
-			// 	link: "/404/",
-			// },
-			// {
-			// 	text: "RSS Feed",
-			// 	link: "/rss.xml/",
-			// },
-			{
-				text: "Get Theme",
-				link: "https://kusa-projects.lemonsqueezy.com/buy/fcfbe185-536a-46c6-a0dd-7f4621a64314",
-				new_tab: true
-			},
-			{
-				text: "KUSA Projects",
-				link: "https://www.kusa-projects.com/",
-				new_tab: true
-			},
-		],
-	},
+	// {
+	// 	text: "Features",
+	// 	dropdown: [
+	// 		// {
+	// 		// 	text: "Features",
+	// 		// 	link: "/features/",
+	// 		// },
+	// 		{
+	// 			text: "Archive",
+	// 			link: "/archive/",
+	// 		},
+	// 		{
+	// 			text: "Tags",
+	// 			link: "/tags/",
+	// 		},
+	// 		{
+	// 			text: "Authors",
+	// 			link: "/authors/",
+	// 		},
+	// 		{
+	// 			text: "Account",
+	// 			link: "/account/",
+	// 		},
+	// 		{
+	// 			text: "Subscribe",
+	// 			link: "/subscribe/",
+	// 		},
+	// 		{
+	// 			text: "Membership",
+	// 			link: "/membership/",
+	// 		},
+	// 		// {
+	// 		// 	text: "Contact",
+	// 		// 	link: "/contact/",
+	// 		// },
+	// 		{
+	// 			text: "FAQ",
+	// 			link: "/faq/",
+	// 		},
+	// 		// {
+	// 		// 	text: "404",
+	// 		// 	link: "/404/",
+	// 		// },
+	// 		// {
+	// 		// 	text: "RSS Feed",
+	// 		// 	link: "/rss.xml/",
+	// 		// },
+	// 		{
+	// 			text: "Get Theme",
+	// 			link: "https://kusa-projects.lemonsqueezy.com/buy/fcfbe185-536a-46c6-a0dd-7f4621a64314",
+	// 			new_tab: true
+	// 		},
+	// 		{
+	// 			text: "KUSA Projects",
+	// 			link: "https://www.kusa-projects.com/",
+	// 			new_tab: true
+	// 		},
+	// 	],
+	// },
 ]
 
 export default navbarSettings;
